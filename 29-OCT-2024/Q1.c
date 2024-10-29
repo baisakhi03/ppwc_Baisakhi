14e89c47013ced32e37d003b7ca954d21c63f64a
baisakhi03 <129980981+baisakhi03@users.noreply.github.com>
2024-10-29 23:36:55 +0530

    Update Q1.c

--- 29-OCT-2024/Q1.c
+++ 29-OCT-2024/Q1.c
@@ -1,11 +1,8 @@
 #include<stdio.h>
 int main(){
     char s[100]; 
-
     printf("Enter a number: ");
     fgets(s, sizeof(s), stdin);
-
-
     for (int i = 0; s[i] != '\0'; i++) {
         if (s[i] == '0') {
             s[i] = '1';
@@ -13,7 +10,6 @@ int main(){
             s[i] = '0';
         }
     }
-
     printf("Modified number is : %s", s);
     return 0;
 }
